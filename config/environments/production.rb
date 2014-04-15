865f9d5fa04df8bc8575308b64ae240b938778f4
ngreim <nathan-greim@uiowa.edu>
2014-04-15 05:20:55 +0000

    removed host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,5 @@
 DeviseExample::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.action_mailer.default_url_options = { :host => 'fundamentals.herokuapp.com' }
   # Code is not reloaded between requests.
   config.cache_classes = true
   config.action_mailer.default_url_options = { :host => 'fundamentalsproject.herokuapp.com' }
