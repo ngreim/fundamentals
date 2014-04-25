fbf816d5cf97699f4c76d7c2c3cdc50e36caa5fc
ngreim <nathan-greim@uiowa.edu>
2014-04-25 06:05:52 +0000

    first attempt at create provider

--- app/controllers/api/v1/providers_controller.rb
+++ app/controllers/api/v1/providers_controller.rb
@@ -1,4 +1,4 @@
-class Api::V1::ProviderSController < ApplicationController
+class Api::V1::ProvidersController < ApplicationController
   skip_before_filter :verify_authenticity_token,
                      :if => Proc.new { |c| c.request.format == 'application/json' }
 
