c80bbf4c358ef195f2a6235224f6503efb8e5730
ngreim <nathan-greim@uiowa.edu>
2014-04-25 04:32:26 +0000

    first attempt at create provider

--- app/controllers/api/v1/user_providers_controller.rb
+++ app/controllers/api/v1/user_providers_controller.rb
@@ -1,9 +1,9 @@
 class Api::V1::UserProvidersController < ApplicationController
-  skip_before_filter :verify_authenticity_token,
+  #skip_before_filter :verify_authenticity_token,
                      :if => Proc.new { |c| c.request.format == 'application/json' }
 
   # Just skip the authentication for now
-   before_filter :authenticate_user!
+   #before_filter :authenticate_user!
 
   respond_to :json
 
