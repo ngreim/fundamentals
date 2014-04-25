a81355eb2f9b5c6a972c8433472d72e7f3c522f3
ngreim <nathan-greim@uiowa.edu>
2014-04-25 07:32:59 +0000

    next attempt at create provider

--- app/controllers/api/v1/create_providers_controller.rb
+++ app/controllers/api/v1/create_providers_controller.rb
@@ -9,7 +9,7 @@ class Api::V1::CreateProvidersController < ApplicationController
   
   def create
 
-  # new_provider = Provider.create!(name: => params[:name] )
+    new_provider = Provider.create!(params[:name])
     render :status => 200,
            :json => { :success => true,
                       :info => "Provider Created",
