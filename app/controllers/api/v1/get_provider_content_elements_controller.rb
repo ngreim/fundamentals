04d40ce6ee144a8658041f5891276c5eb2dd5e04
ngreim <nathan-greim@uiowa.edu>
2014-04-26 19:19:18 +0000

    implementing the content elements route

--- app/controllers/api/v1/get_provider_content_elements_controller.rb
+++ app/controllers/api/v1/get_provider_content_elements_controller.rb
@@ -1,4 +1,4 @@
-class Api::V1::GetProviderContentElements < ApplicationController
+class Api::V1::GetProviderContentElementsController < ApplicationController
   skip_before_filter :verify_authenticity_token,
                      :if => Proc.new { |c| c.request.format == 'application/json' }
 
