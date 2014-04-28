10aa0f8e103cb6a50af3a985ecef5d681f87f874
ngreim <nathan-greim@uiowa.edu>
2014-04-28 02:29:34 +0000

    working on delete formats

--- db/migrate/201440218023206_add_column_to_content_element2.rb
+++ db/migrate/201440218023206_add_column_to_content_element2.rb
@@ -1,5 +1,5 @@
 class AddColumnToContentElement2 < ActiveRecord::Migration
   def change
-    add_column :content_element, :format_id, :integer
+    add_column :content_elements, :format_id, :integer
   end
 end
