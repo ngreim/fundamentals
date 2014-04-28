f2e543d9deda882fa85fa276e6ae6ca810ae04b0
ngreim <nathan-greim@uiowa.edu>
2014-04-28 03:07:56 +0000

    added return of provider subscription types

--- app/controllers/api/v1/get_provider_subscription_types_controller.rb
+++ app/controllers/api/v1/get_provider_subscription_types_controller.rb
@@ -1,4 +1,4 @@
-class Api::V1::GetProviderSubscriptionTypes < ApplicationController
+class Api::V1::GetProviderSubscriptionTypesController < ApplicationController
   skip_before_filter :verify_authenticity_token,
                      :if => Proc.new { |c| c.request.format == 'application/json' }
 
