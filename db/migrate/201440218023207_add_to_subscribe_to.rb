dc8a953500202003655a45dcf8c8b0cd2728d92a
ngreim <nathan-greim@uiowa.edu>
2014-04-28 21:06:30 +0000

    subscribe to end date addition

--- db/migrate/201440218023207_add_to_subscribe_to.rb
+++ db/migrate/201440218023207_add_to_subscribe_to.rb
@@ -1,5 +1,6 @@
 class AddColumnToContentElement2 < ActiveRecord::Migration
   def change
     add_column :subscribe_tos, :subscription_length, :integer
+    add_column :subscribe_tos, :end_date, :integer
   end
 end
