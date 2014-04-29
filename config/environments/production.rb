ba461b29a0fdba5e79ca5e1ab405b4dd43a36913
ngreim <nathan-greim@uiowa.edu>
2014-04-29 06:21:24 +0000

    adding email password recovery

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,7 @@ DeviseExample::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
   
-  config.action_mailer.default_url_options = { :host => 'Fundamentals.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'fundamentalsproject.herokuapp.com' }
   
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
