e66d813f0bad133de8fb924ed60e56edaa4e9f08
ngreim <nathan-greim@uiowa.edu>
2014-04-29 06:59:08 +0000

    added table for ended subscriptions

---
+++ db/migrate/201440218023211_ended_subscriptions.rb
@@ -0,0 +1,13 @@
+class EndedSubscriptions < ActiveRecord::Migration
+  def change
+    create_table :ended_subscriptions do |t|
+      t.integer :user_id
+      t.integer :provider_id
+      t.integer :subscription_id
+      t.integer :id
+      t.integer :end_date
+      t.string :subscription_type
+      t.time :end_date_time
+    end
+  end
+end
